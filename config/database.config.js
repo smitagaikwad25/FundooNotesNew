a15ab690a21089a430ce216f4c8c5a368a90c8b5
smitagaikwad25 <gaikwadsmita021@gmail.com>
2021-11-24 16:52:54 +0530

    Added: env variables

--- config/database.config.js
+++ config/database.config.js
@@ -1,3 +1,15 @@
-module.exports = {
-    url: 'mongodb://localhost:27017/fundooNotes'
+
+const mongoose = require('mongoose');
+require('dotenv').config()
+
+const dbconnect = () => {
+    mongoose.connect(process.env.DB_URl, {
+        useNewUrlParser: true
+    }).then(() => {
+        console.log("Successfully connected to the database");
+    }).catch(err => {
+        console.log('Could not connect to the database. Exiting now...', err);
+        process.exit();
+    })
 }
+module.exports = dbconnect
